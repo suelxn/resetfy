4d175b3ab6fada1f5597d6fd4e0005e6cbf467ad
Suelen Dias <suelenanjos16@gmail.com>
2026-06-21 21:55:14 -0300

    cria estrutura inicial da interface Resetfy

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export function Home() {
+  return (
+    <main>
+      <h1>Resetfy</h1>
+
+      <p>
+        Seu assistente de pausas inteligentes
+      </p>
+
+      <button>
+        Iniciar
+      </button>
+
+    </main>
+  )
+}
